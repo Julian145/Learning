9be46b62950c840ba6eaa29c91ca11b2f286b254
JaroldJulian <124736927+Julian145@users.noreply.github.com>
2023-02-13 13:08:37 -0800

    Modules update
    Learning how to use modules

---
+++ Fundamentales Python 2/Módulos, paquetes y PIP/Módulos/main2.py
@@ -0,0 +1,9 @@
+from sys import path
+
+
+import extra.good.best.sigma
+from extra.good.best.tau import funT
+
+print(extra.good.best.sigma.funS())
+print(funT())
+
--- Fundamentales Python 2/Módulos, paquetes y PIP/main2.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('..\\packages')
-
-import extra.iota
-print(extra.iota.funI())
-
